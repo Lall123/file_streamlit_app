2dbc283eebb2edc868b22ca81fb7e628ac7d6725
Lall123 <109822517+Lall123@users.noreply.github.com>
2022-07-23 12:13:48 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text('🥣 🥗 🐔 🍞 all of them ')
 streamlit.text('🥣 🥗 🍞 all of them ')
 streamlit.text('🥣 🥗  all of them ')
 
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
