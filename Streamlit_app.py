24667ad60f316e43644779038cf2193340f46028
Lall123 <109822517+Lall123@users.noreply.github.com>
2022-07-23 11:05:03 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,3 +2,4 @@ Import streamlit
 streamlit.title('dexter and Max are playing')
 streamlit.text('emojis not found  🍠')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
